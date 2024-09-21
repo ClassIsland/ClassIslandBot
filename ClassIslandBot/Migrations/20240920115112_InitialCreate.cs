0c1ebf8fda77af1c546948cf4b6b177bf1af441f
WRC <hello_wrc@outlook.com>
2024-09-21 22:19:36 +0800

    feat: 添加迁移信息

---
+++ ClassIslandBot/Migrations/20240920115112_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassIslandBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiscussionAssociations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RepoId = table.Column<string>(type: "TEXT", nullable: false),
+                    DiscussionId = table.Column<string>(type: "TEXT", nullable: false),
+                    IssueId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscussionAssociations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscussionAssociations");
+        }
+    }
+}
---
+++ ClassIslandBot/Migrations/20240920122448_AddIsTracking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassIslandBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTracking",
+                table: "DiscussionAssociations",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTracking",
+                table: "DiscussionAssociations");
+        }
+    }
+}
